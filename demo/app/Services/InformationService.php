970cebe639928fbcecffc0141c79a11a7bcd8421
Júnior <jrcantador@gmail.com>
2023-06-24 17:51:53 -0300

    Add tables

--- demo/app/Services/InformationService.php
+++ demo/app/Services/InformationService.php
@@ -138,12 +138,16 @@ class InformationService
         $pages = $pdf->getPages();
         $infos = [];
 
+
         foreach ($pages as $page) {
             $infos = array_merge($infos, array_map(function ($row) {
                 return  $row[1];
             }, $page->getDataTm()));
         }
-        $heads = [
+
+        dd($infos);
+
+        $head01 = [
             "1 - Registro ANS" => [],
             "3 - Nome da Operadora" => [],
             "4 - CNPJ da Operadora" => [],
@@ -157,41 +161,46 @@ class InformationService
             "12 - Código da Glosa do Protocolo" => []
         ];
 
+        $head02 = [];
+
+        $head03 = [];
+
+        // Primeira Página
         foreach ($infos as $key => $info) {
             $padrao = "/1 - Registro ANS/";
             if (preg_match($padrao, $info)) {
-                $heads["1 - Registro ANS"][] = $infos[$key + 1];
+                $head01["1 - Registro ANS"][] = $infos[$key + 1];
                 continue;
             }
 
             $padrao = "/3 - Nome da Operadora/";
             if (preg_match($padrao, $info)) {
-                $heads["3 - Nome da Operadora"][] =
+                $head01["3 - Nome da Operadora"][] =
                     $row[] = $infos[$key + 1];
                 continue;
             }
 
             $padrao = "/4 - CNPJ da Operadora/";
             if (preg_match($padrao, $info)) {
-                $heads["4 - CNPJ da Operadora"][] = $infos[$key + 1];
+                $head01["4 - CNPJ da Operadora"][] = $infos[$key + 1];
                 continue;
             }
 
             $padrao = "/TOTAL GERAL/";
             if (preg_match($padrao, $info)) {
-                $heads["5 - Data de Emissão"][] = $infos[$key + 1];
+                $head01["5 - Data de Emissão"][] = $infos[$key + 1];
                 continue;
             }
 
             $padrao = "/6 - Código na Operadora/";
             if (preg_match($padrao, $info)) {
-                $heads["6 - Código na Operadora"][] = $infos[$key - 1];
+                $head01["6 - Código na Operadora"][] = $infos[$key - 1];
                 continue;
             }
 
             $padrao = "/7 - Nome do Contratado/";
             if (preg_match($padrao, $info)) {
-                $heads["7 - Nome do Contratado"][] = $infos[$key - 1];
+                $head01["7 - Nome do Contratado"][] = $infos[$key - 1];
                 continue;
             }
 
@@ -202,7 +211,7 @@ class InformationService
             }
             $padrao = "/9 - Número do Lote/";
             if (preg_match($padrao, $info)) {
-                $heads["10 - Nº do Protocolo (Processo)"][] = $infos[$key + 1];
+                $head01["10 - Nº do Protocolo (Processo)"][] = $infos[$key - 1];
                 continue;
             }
 
@@ -214,27 +223,161 @@ class InformationService
 
             $padrao = "/12 - Código da Glosa do Protocolo/";
             if (preg_match($padrao, $info)) {
-                $heads["8 - Código CNES"][] = $infos[$key + 1];
-                $heads["12 - Código da Glosa do Protocolo"][] = $infos[$key + 2];
+                $head01["8 - Código CNES"][] = $infos[$key + 1];
+                $head01["12 - Código da Glosa do Protocolo"][] = $infos[$key + 2];
                 continue;
             }
         }
 
+        // Primeira Página
+        foreach ($infos as $key => $info) {
+            $padrao = "/13 - Número da Guia no Prestador/";
+            if (preg_match($padrao, $info)) {
+                $head02["13 - Número da Guia no Prestador"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/14 - Número da Guia Atribuido pela Operadora/";
+            if (preg_match($padrao, $info)) {
+                $head02["14 - Número da Guia Atribuido pela Operadora"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/15 - Senha/";
+            if (preg_match($padrao, $info)) {
+                $head02["14 - Número da Guia Atribuido pela Operadora"][] = $infos[$key - 1];
+                continue;
+            }
+
+            $padrao = "/16 - Nome do Beneficiário/";
+            if (preg_match($padrao, $info)) {
+                $head02["16 - Nome do Beneficiário"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/17 - Número da Carteira/";
+            if (preg_match($padrao, $info)) {
+                $head02["16 - Nome do Beneficiário"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/18 - Data Início do Faturamento/";
+            if (preg_match($padrao, $info)) {
+                $head02["18 - Data Início do Faturamento"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/19 - Hora Início do Faturamento/";
+            if (preg_match($padrao, $info)) {
+                $head02["19 - Hora Início do Faturamento"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/20 - Data Fim do Faturamento/";
+            if (preg_match($padrao, $info)) {
+                $head02["20 - Data Fim do Faturamento"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/21 - Hora Fim do Faturamento/";
+            if (preg_match($padrao, $info)) {
+                $head02["21 - Hora Fim do Faturamento"][] = $infos[$key - 1];
+                continue;
+            }
+            $padrao = "/22 - Código da Glosa da Guiao/";
+            if (preg_match($padrao, $info)) {
+                $head02["22 - Código da Glosa da Guia"][] = $infos[$key - 1];
+                continue;
+            }
+            
+        }
+
+           
+
+        // Dados da Guia
+        $tableInfos = [];
+        $initiaded = false;
+        $countIndex = 0;
+        foreach ($infos as $key => $info) {
+            if (
+                !preg_match("/22 - Código da Glosa da Guia/", $info) &&
+                !$initiaded
+            ) {
+                continue;
+            }
+
+            if (preg_match("/22 - Código da Glosa da Guia/", $info)) {
+                $countIndex++;
+                $initiaded = true;
+                $tableInfos[] = $infos[$key + 1];
+            } elseif ($initiaded && !preg_match("/23 - Data de/", $info)) {
+                $tableInfos[] = $infos[$key + 1];
+            } elseif ($initiaded && preg_match("/23 - Data de/", $info)) {
+                $countInfos = (count($tableInfos) - 1) / 10;
+                $values = array_chunk($tableInfos, $countInfos);
+                foreach ($values as $index => $value) {
+                    foreach ($value as $infoField) {
+                        if ($index == 0) {
+                            $head03["23 - Data de realização"][] = $infoField;
+                        } elseif ($index == 1) {
+                            $head03["24 - Tabela /Item assistencial"][] = $infoField;
+                        } elseif ($index == 2) {
+                            $head03["25 - Código Procedimento"][] = $infoField;
+                        } elseif ($index == 3) {
+                            $head03["26 - Descrição"][] = $infoField;
+                        } elseif ($index == 4) {
+                            $head03["27 - Grau de Participação - Descrição"][] = $infoField;
+                        } elseif ($index == 5) {
+                            $head03["28 - Valor informado"][] = $infoField;
+                        } elseif ($index == 6) {
+                            $head03["29 -Quantidade executada"][] = $infoField;
+                        } elseif ($index == 7) {
+                            $head03["30 - Valor processado"][] = $infoField;
+                        } elseif ($index == 8) {
+                            $head03["31 - Valor liberado"][] = $infoField;
+                        } elseif ($index == 9) {
+                            $head03["32 - Valor glosao"][] = $infoField;
+                        }
+                    }
+                }
+                $initiaded = false;
+                $tableInfos = [];                
+            }
+        }
 
         $file = fopen("storage/file.csv", "w");
-        fputcsv($file, array_keys($heads), ",");
+        fputcsv($file, array_keys($head01), ",");
 
         $countRows = 0;
-        foreach ($heads as $key => $head) {
+        foreach ( $head01  as $key => $head) {
             $countRows += count($head);
         }
-        $countRows = $countRows / count($heads);
+        $countRows = $countRows / count($head01);
+
 
         $rows = [];
         for ($i = 0; $i  < $countRows; $i++) {
             $row = [];
-            foreach ($heads as $key => $head) {
-                $row[] = $head[$i];
+            foreach ($head01 as $key => $head) {
+                $row[] = (count($head) - 1) >= $i ? $head[$i] : "";
+            }
+            $rows[] = $row;
+        }
+
+        foreach ($rows as $row) {
+            fputcsv($file, $row, ",");
+        }
+
+        
+
+        fputcsv($file, [], ",");
+
+        fputcsv($file, array_keys($head03), ",");
+        $countRows = 0;
+        foreach ( $head03  as $key => $head) {
+            $countRows += count($head);
+        }
+        $countRows = $countRows / count($head01);
+
+
+        $rows = [];
+        for ($i = 0; $i  < $countRows; $i++) {
+            $row = [];
+            foreach ($head03 as $key => $head) {
+                $row[] = (count($head) - 1) >= $i ? $head[$i] : "";
             }
             $rows[] = $row;
         }
@@ -244,6 +387,7 @@ class InformationService
         }
 
         fclose($file);
+       
 
         return "Concluído";
     }
